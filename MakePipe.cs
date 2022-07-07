2657719cf6792782b7e6655b1754c484e0417c10
taehee0 <107236055+taehee0@users.noreply.github.com>
2022-07-08 06:40:47 +0900

    Add files via upload

---
+++ MakePipe.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject pipe;
+    public float timeDiff;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeDiff)
+        { 
+           GameObject newpipe = Instantiate(pipe);
+           newpipe.transform.position = new Vector3(6, Random.Range(-1.7f, 5.7f), 0);
+           timer = 0;
+           Destroy(newpipe,10.0f);
+       
+        } 
+    }
+}
